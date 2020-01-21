5365080b9afad31dc42aa4d077cc3eaafe5b9e02
taterntots <matthew.a.weidner@gmail.com>
2020-01-21 16:34:00 -0500

    created endpoints for both getting a list of cars and creating new ones. MVP achieved!

--- routers/carsRouter.js
+++ routers/carsRouter.js
@@ -5,13 +5,30 @@ const router = express.Router();
 
 //gets a list of cars
 router.get('/', (req, res) => {
-  // db.get()
-  //   .then(cars => {
-  //     res.status(200).json(cars);
-  //   })
-  //   .catch(error => {
-  //     res.status(500).json({ errorMessage: 'There was an error retrieving the list of cars' })
-  //   })
+  db.select('*')
+    .from('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error retrieving the list of cars' })
+    })
+})
+
+router.post('/', (req, res) => {
+  db.insert(req.body)
+    .into('cars')
+    .then(ids => {
+      db.select('*')
+        .from('cars')
+        .where({ VIN: ids[0] })
+        .then(newCar => {
+          res.status(201).json(newCar);
+        })
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Failed to add new car data' })
+    })
 })
 
 module.exports = router;
